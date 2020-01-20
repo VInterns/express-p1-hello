f0630a3641b95bd7c86e255b486d761e4b122c60
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-01-20 12:01:22 +0200

    connected to mongo

--- somaa-project/server.js
+++ somaa-project/server.js
@@ -1,8 +1,49 @@
 const express = require('express');
+const mongoClient = require('mongodb').MongoClient;
 
 // Create the express app
 const server = express();
 
+// connect to db
+mongoClient.connect("mongodb://localhost:27017/", (err, db) => {
+
+    if(err) {throw err}
+    else{
+
+        
+        var database = db.db('vodafone-test');
+        database.createCollection('interns', function(err, res){
+            if (err) console.log("[-] Couldn't Create a Collection");
+            else{
+                console.log('[+] Collection Created');
+                var myobj = [
+                    { name: 'John', address: 'Highway 71'},
+                    { name: 'Peter', address: 'Lowstreet 4'},
+                    { name: 'Amy', address: 'Apple st 652'},
+                    { name: 'Hannah', address: 'Mountain 21'},
+                    { name: 'Michael', address: 'Valley 345'},
+                    { name: 'Sandy', address: 'Ocean blvd 2'},
+                    { name: 'Betty', address: 'Green Grass 1'},
+                    { name: 'Richard', address: 'Sky st 331'},
+                    { name: 'Susan', address: 'One way 98'},
+                    { name: 'Vicky', address: 'Yellow Garden 2'},
+                    { name: 'Ben', address: 'Park Lane 38'},
+                    { name: 'William', address: 'Central st 954'},
+                    { name: 'Chuck', address: 'Main Road 989'},
+                    { name: 'Viola', address: 'Sideway 1633'}
+                ];
+                database.collection("interns").insertMany(myobj, (err) => {
+                    if(err) console.log("[-] Couldn't Insert Object")
+                    console.log("[+] Object Inserted");
+                })
+            }
+        })
+    }
+
+
+
+
+})
 
 // Setup the port
 const PORT = 3000;
